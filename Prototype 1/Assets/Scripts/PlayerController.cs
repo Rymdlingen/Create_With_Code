41baa7a4ac66235d8f302298eb7e951c0f9ec895
Rymdlingen <71494058+Rymdlingen@users.noreply.github.com>
2021-01-18 15:57:39 +0100

    Lesson 6.2 completed

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     // Private variables.
-    [SerializeField] float speed = 5.0f;
+    [SerializeField] float speed;
     [SerializeField] float turnSpeed = 25.0f;
+    [SerializeField] float horsePower = 0;
     private float horizontalInput;
     private float forwardInput;
+    private Rigidbody playerRb;
+    [SerializeField] GameObject centerOfMass;
+    [SerializeField] TextMeshProUGUI speedometerText;
+    [SerializeField] TextMeshProUGUI rpmText;
+    [SerializeField] float rpm;
+    [SerializeField] List<WheelCollider> allWheels;
+    [SerializeField] int wheelsOnGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.centerOfMass = centerOfMass.transform.position;
+    }
 
     // Update is called once per frame
     void FixedUpdate()
@@ -17,10 +33,44 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
 
-        // Move the vehicle forward.
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        if (IsOnGround())
+        {
+            // Move the vehicle forward.
+            //transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+            playerRb.AddRelativeForce(Vector3.forward * horsePower * forwardInput);
 
-        // Turn the vehicle.
-        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+            // Turn the vehicle.
+            transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+            // Get the speed of the vehicle and display it on the screen.
+            speed = Mathf.RoundToInt(playerRb.velocity.magnitude * 3.6f);
+            speedometerText.SetText("Speed: " + speed + " km/h");
+
+            rpm = (speed % 30) * 80;
+            rpmText.SetText("RPM: " + rpm);
+        }
+    }
+
+    bool IsOnGround()
+    {
+        wheelsOnGround = 0;
+
+        foreach (WheelCollider wheel in allWheels)
+        {
+            if (wheel.isGrounded)
+            {
+                wheelsOnGround++;
+            }
+        }
+
+        if (wheelsOnGround == 4)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
