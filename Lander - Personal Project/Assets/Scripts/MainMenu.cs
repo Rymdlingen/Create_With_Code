8c2c5af88b4fae8d72604882c534ec007edcfc83
Rymdlingen <71494058+Rymdlingen@users.noreply.github.com>
2021-01-22 18:23:53 +0100

    The main menu now works.
    The meny has play, starts the game, and quit, closes the game. I could add a menu for settings if I need it.

---
+++ Lander - Personal Project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    // Activated by pressing the play button in the main menu.
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Lander");
+    }
+
+    // Activated by pressing the quit button in the main menu.
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
