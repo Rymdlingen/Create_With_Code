ffce082866f468454c4055e38172552a486c9534
Rymdlingen <71494058+Rymdlingen@users.noreply.github.com>
2021-02-25 00:07:37 +0100

    Fixed the size of the particles for the powerups.

---
+++ Lander - Personal Project/Assets/Scripts/ChangeParticelSize.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeParticelSize : MonoBehaviour
+{
+    private Camera sceneCamera;
+    [SerializeField] private ParticleSystem[] particles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneCamera = GameObject.Find("Scene Camera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Change the size of the particles based on what camera is active.
+        if (sceneCamera.isActiveAndEnabled)
+        {
+            foreach (ParticleSystem particle in particles)
+            {
+                particle.startSize = 1;
+            }
+        }
+        else
+        {
+            foreach (ParticleSystem particle in particles)
+            {
+                particle.startSize = 3;
+            }
+        }
+    }
+}
